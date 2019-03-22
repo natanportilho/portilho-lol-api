4d058ff4fc0ee0a54de9f346b12974a345e69d93
Natan Portilho <>
2019-03-21 21:41:38 -0300

    Implementing LolChampionService

--- src/test/java/com/portilho/lol/api/portilhololapi/service/LolChampionServiceTest.java
+++ src/test/java/com/portilho/lol/api/portilhololapi/service/LolChampionServiceTest.java
@@ -1,5 +1,24 @@
 package com.portilho.lol.api.portilhololapi.service;
 
+import com.portilho.lol.api.portilhololapi.service.champion.LolChampionService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class LolChampionServiceTest
 {
+    @Resource
+    private LolChampionService championService;
+
+    @Test
+    public void getAllChampionsTest()
+    {
+        Assert.assertEquals(143, championService.getAllChampions().size());
+    }
 }
