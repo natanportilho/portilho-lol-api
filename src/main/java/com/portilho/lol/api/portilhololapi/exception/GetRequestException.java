075a7cee2bb051f8443ade5a7f307cb26bfbab6a
Natan Portilho <>
2019-03-21 21:38:03 -0300

    Adding new exception for get request

---
+++ src/main/java/com/portilho/lol/api/portilhololapi/exception/GetRequestException.java
@@ -0,0 +1,9 @@
+package com.portilho.lol.api.portilhololapi.exception;
+
+public class GetRequestException extends RuntimeException
+{
+    public GetRequestException(String message)
+    {
+        super(message);
+    }
+}
