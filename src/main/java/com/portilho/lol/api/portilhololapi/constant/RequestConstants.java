b64359c681644f9075ae2b2250448b0f0c7785f3
Natan Portilho <>
2019-03-24 11:05:46 -0300

    Creating private constructor for constansts class

--- src/main/java/com/portilho/lol/api/portilhololapi/constant/RequestConstants.java
+++ src/main/java/com/portilho/lol/api/portilhololapi/constant/RequestConstants.java
@@ -2,9 +2,13 @@ package com.portilho.lol.api.portilhololapi.constant;
 
 public class RequestConstants
 {
+    private RequestConstants()
+    {
+    }
+
     public static final String SUMMONER_INFO_URL = "https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
     public static final String GET = "GET";
-    public static final String RIOT_TOKEN = "RGAPI-e082f18f-4078-42c0-9065-0b1b8016c81b";
+    public static final String RIOT_TOKEN = "RGAPI-74989197-062a-41df-ae81-feac4028736f";
     public static final String MATCHES_FOR_USER_URL = "https://br1.api.riotgames.com/lol/match/v4/matchlists/by-account/";
     public static final String CHAMPIONS_URL = "http://ddragon.leagueoflegends.com/cdn/9.5.1/data/en_US/champion.json";
     public static final String MATCH_BY_ID_URL = "https://br1.api.riotgames.com/lol/match/v4/matches/";
