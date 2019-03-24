89458fcd8c11660cec5f4ecf7773bcd71c415400
Natan Portilho <>
2019-03-24 11:06:40 -0300

    Creating tests class for LolMatchesFacade

---
+++ src/test/java/com/portilho/lol/api/portilhololapi/facade/LolMatchesFacadeTest.java
@@ -0,0 +1,23 @@
+package com.portilho.lol.api.portilhololapi.facade;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LolMatchesFacadeTest
+{
+    @Resource
+    private LolMatchesFacade matchesFacade;
+
+    @Test
+    public void getUserMatchHistoryTest()
+    {
+        Assert.assertNotNull(matchesFacade.getUserMatchHistory("NatanPortilho"));
+    }
+}
